6f461520dd0802a9c3d03ca2ee114314e8212c69
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:28:39 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 ./configure --prefix=/usr                      \
             --without-clisp                    \
             --without-maximum-compile-warnings
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-install -v -m755 -d ${SHED_FAKEROOT}/usr/share/doc/swig-3.0.12
-cp -v -R Doc/* ${SHED_FAKEROOT}/usr/share/doc/swig-3.0.12
+make -j $SHED_NUM_JOBS
+make DESTDIR=${SHED_FAKE_ROOT} install
+install -v -m755 -d ${SHED_FAKE_ROOT}/usr/share/doc/swig-3.0.12
+cp -v -R Doc/* ${SHED_FAKE_ROOT}/usr/share/doc/swig-3.0.12
