00ef662a8aadaf9eb07230a8e34a55bb979a4c39
Auston Stewart <auston@shedbuilt.net>
2018-05-31 09:45:08 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+# Configure
 ./configure --prefix=/usr                      \
             --without-clisp                    \
-            --without-maximum-compile-warnings
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
-install -v -m755 -d ${SHED_FAKE_ROOT}/usr/share/doc/swig-3.0.12
-cp -v -R Doc/* ${SHED_FAKE_ROOT}/usr/share/doc/swig-3.0.12
+            --without-maximum-compile-warnings &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    install -vdm755 "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}" &&
+    cp -v -R Doc/* "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+fi
